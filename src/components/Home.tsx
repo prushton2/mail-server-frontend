673fd3a1f23343c20c9ec193f2d4908c54624b67
prushton2 <peterrushton418@gmail.com>
2023-10-25 20:47:31 -0400

    Homepage progress

---
+++ src/components/Home.tsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+import './Home.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='header'>
+        Jacks Mail Service
+        <br />
+        <div className='headermenu'>
+            Logo Here
+        </div>
+        <div className='headermenu'>
+            <button className='headerButton'>About</button> 
+        </div>
+        <div className='headermenu'>
+            <button className='headerButton'>Sign Up</button> 
+        </div>
+        <div className='headermenu'>
+            <button className='headerButton'>Log In</button> 
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Home from './components/Home.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter(
+  [{path: "/", element: <Home />}]
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
