154675a977872b0cd54d72af612f370137572c10
prushton2 <peterrushton418@gmail.com>
2023-10-26 00:08:56 -0400

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './components/Home.tsx'
+import Mail from './components/Mail.tsx'
+import Login from './components/Login.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
 const router = createBrowserRouter(
-  [{path: "/", element: <Home />}]
+  [
+    {path: "/", element: <Home />},
+    {path: "/mail", element: <Mail />},
+    {path: "/login", element: <Login />}
+  ],
 )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
